0a11347018b211c99fb3f7fd9999acad89935e3a
Mariofte <150403921+Mariofte@users.noreply.github.com>
2024-10-13 23:41:57 -0600

    Optimizada la sintaxis

--- Baked/Class_Hoja.py
+++ Baked/Class_Hoja.py
@@ -1,19 +1,24 @@
+# importacion de modulos
 import gspread as gs
 import pandas as pd
 import streamlit as st
+# importacion de clases
 from oauth2client.service_account import ServiceAccountCredentials as service
+from gspread.exceptions import APIError
 
 class Hoja:
+    # argumentos a pedir
     def __init__ (self,bot,scope,id,hoja):
         self.bot = bot 
         self.scope = scope
         self.id = id
         self.hoja = hoja
     
+    
     def leer (self):
         try:
             creds = service.from_json_keyfile_name(filename = self.bot, scopes = self.scope)
-            
+
             client = gs.authorize(credentials = creds)
 
             sheet = client.open_by_key(key = self.id)
@@ -29,12 +34,13 @@ class Hoja:
         except FileNotFoundError as fnf_error:
             st.error(f"Archivo de credenciales no encontrado: {fnf_error}")
         
-        except gs.exceptions.APIError as api_error:
+        except APIError as api_error:
             st.error(f"Error de la API de Google Sheets: {api_error}")
         
         except Exception as error:
             st.error(f"Hubo un error: {error}")
             
+    # No funciones sin declarar el sheets como vicible para cualquiera con el link
     def leer_2 (self):
         try:
             url = f'https://docs.google.com/spreadsheets/d/{self.id}/export?fromat=csv'
@@ -60,12 +66,13 @@ class Hoja:
         except FileNotFoundError as fnf_error:
             st.error(f"Archivo de credenciales no encontrado: {fnf_error}")
         
-        except gs.exceptions.APIError as api_error:
+        except APIError as api_error:
             st.error(f"Error de la API de Google Sheets: {api_error}")
         
         except Exception as error:
             st.error(f"Hubo un error: {error}")
         
+    # En proceso de mejora
     def limpiar (self,rango):
         try:
             creds = service.from_json_keyfile_name(filename = self.bot, scopes = self.scope)
@@ -81,7 +88,7 @@ class Hoja:
         except FileNotFoundError as fnf_error:
             st.error(f"Archivo de credenciales no encontrado: {fnf_error}")
         
-        except gs.exceptions.APIError as api_error:
+        except APIError as api_error:
             st.error(f"Error de la API de Google Sheets: {api_error}")
         
         except Exception as error:
